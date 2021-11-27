18c6059d4f869f1c89a03cb3e71373bb71bbd489
Andrew Hunter <andrew@logicalshift.co.uk>
2021-11-27 16:34:03 +0000

    Add tests for the new rope concat() function

--- src/rope_binding/tests.rs
+++ src/rope_binding/tests.rs
@@ -42,3 +42,38 @@ fn pull_from_mutable_binding() {
     assert!(rope_copy.len() == 4);
     assert!(rope_copy.read_cells(0..4).collect::<Vec<_>>() == vec![1, 2, 3, 4]);
 }
+
+#[test]
+fn concatenate_ropes() {
+    // Create a LHS and RHS rope and a concatenation of both
+    let mut lhs             = RopeBindingMut::<usize, ()>::new();
+    let mut rhs             = RopeBindingMut::<usize, ()>::new();
+    let concat              = lhs.concat(&rhs);
+
+    // We need to wait for the changes to arrive on the concatenated rope to avoid racing when reading back
+    let mut follow_concat   = concat.follow_changes();
+
+    // Add to LHS
+    lhs.replace(0..0, vec![1, 2, 3]);
+    executor::block_on(async { follow_concat.next().await });
+    println!("{:?}", concat.read_cells(0..3).collect::<Vec<_>>());
+    assert!(concat.read_cells(0..3).collect::<Vec<_>>() == vec![1, 2, 3]);
+
+    // Add to RHS
+    rhs.replace(0..0, vec![10, 11, 12]);
+    executor::block_on(async { follow_concat.next().await });
+    println!("{:?}", concat.read_cells(0..6).collect::<Vec<_>>());
+    assert!(concat.read_cells(0..6).collect::<Vec<_>>() == vec![1, 2, 3, 10, 11, 12]);
+
+    // Edit LHS
+    lhs.replace(1..2, vec![4, 5, 6]);
+    executor::block_on(async { follow_concat.next().await });
+    println!("{:?}", concat.read_cells(0..8).collect::<Vec<_>>());
+    assert!(concat.read_cells(0..8).collect::<Vec<_>>() == vec![1, 4, 5, 6, 3, 10, 11, 12]);
+
+    // Edit RHS
+    rhs.replace(1..2, vec![20, 21, 22]);
+    executor::block_on(async { follow_concat.next().await });
+    println!("{:?}", concat.read_cells(0..10).collect::<Vec<_>>());
+    assert!(concat.read_cells(0..10).collect::<Vec<_>>() == vec![1, 4, 5, 6, 3, 10, 20, 21, 22, 12]);
+}
